f56d2b3cebe0f34cf28fcb60e93a090b4630b9f6
John-Philip Johansson <john-philip.johansson@episerver.com>
2020-05-31 09:37:00 +0200

    Show what special day it is

--- index.js
+++ index.js
@@ -6,4 +6,11 @@ const fikaDate = document.fika.kalender[month][date];
 
 const $fika = document.getElementById("fika-node");
 
-$fika.innerText = fikaDate ? fikaDate.coffee : "precis vad du vill!"
+// Celebrate a special day
+if (fikaDate) {
+    $fika.innerHTML = `<h3>${fikaDate.coffee}</h3> <p>för det är <strong><a href="${fikaDate.source}">${fikaDate.name}</a></strong>!</p>`;
+}
+// Or fall back
+else {
+    $fika.innerHTML = "<h3>precis vad du vill!</h3>";
+}
