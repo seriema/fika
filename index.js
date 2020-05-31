c27fe452f624a4e64f1ce394e3c60cd4ee144779
John-Philip Johansson <john-philip.johansson@episerver.com>
2020-05-31 09:14:33 +0200

    Revert "Add random desserts for days without anything special"
    It got too distracting. From user comments, they see the dessert and
    think that's it. It looses the charm and focus of showing you a special
    day and being very obvious when a date is missing so that you _want_ to
    click the "add date" link.
    
    This reverts commit 7c2641e86f8640f2cff544588060996a0eb6a757.

--- index.js
+++ index.js
@@ -6,13 +6,4 @@ const fikaDate = document.fika.kalender[month][date];
 
 const $fika = document.getElementById("fika-node");
 
-// Celebrate a special day
-if (fikaDate) {
-    $fika.innerText = `${fikaDate.coffee} för att fira ${fikaDate.name}!`;
-}
-// Or fall back to any dessert that goes with coffee
-else {
-    const desserter = document.fika.dessert;
-    const randomFika = desserter[Math.floor(Math.random() * desserter.length)];
-    $fika.innerText = randomFika.coffee;
-}
+$fika.innerText = fikaDate ? fikaDate.coffee : "precis vad du vill!"
